845ce8dba02d37432d44847468a6b11457ff4db8
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-05-15 22:31:07 +0200

    k=97/5

--- examples/coupled_system.jl
+++ examples/coupled_system.jl
@@ -134,14 +134,14 @@ end
 begin
     function WindDiodes(; name, R_L = 10.0, p = 4)
 
-        @named aero = Constant(k = -97/10)
+        @named aero = Constant(k = -97/5)
         #@named aero = Ramp(offset=0.0, height=-97.24, duration=15.0, start_time=0.0)
         @named gen = PMSG(p = p)
         @named zbridge = DiodeBridge()
         @named rload = Resistor(R = R_L)
         @named gnd = Ground()   # fija V_neutro = 0
-        @named leak_p = Resistor(R = 1.0e8)
-        @named leak_n = Resistor(R = 1.0e8)
+        @named leak_p = Resistor(R = 1.0e6)
+        @named leak_n = Resistor(R = 1.0e6)
         @named cap = Capacitor(C = 1.0e-6, v = 0.0)
 
         eqs = [
@@ -175,7 +175,7 @@ end
 sysc = mtkcompile(sys; warn_initialize_determined = false)
 prob = ODEProblem(sysc, [], (0.0, 3.0); warn_initialize_determined = false)
 
-sol = solve(prob, RadauIIA5(autodiff=AutoForwardDiff(), κ = 0.005), abstol = 1e-7, reltol = 1e-9, saveat = 0.0001)
+sol = solve(prob, RadauIIA5(autodiff=AutoForwardDiff(), κ = 0.005), abstol = 1e-8, reltol = 0.5e-9, saveat = 0.0001)
 
 time    = sol.t
 ω_rpm   = sol[sys.gen.ωₘ] .* (30 / π)    # rad/s → RPM
