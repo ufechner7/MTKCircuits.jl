01cbe591edf5dce10a32d464af0389895ac1069f
Uwe Fechner <fechner@aenarete.eu>
2026-05-15 13:46:16 +0200

    Cleanup

--- examples/synchronous_machine.jl
+++ examples/synchronous_machine.jl
@@ -36,19 +36,15 @@ function PMSG(;
         D(ib) ~ (1 / L) * (Ψ * p * ωₘ * sin(p * Θ - 2π / 3) - R * ib - (pin_b.v - pin_n.v)),
         D(ic) ~ (1 / L) * (Ψ * p * ωₘ * sin(p * Θ + 2π / 3) - R * ic - (pin_c.v - pin_n.v)),
 
-
         Tₑ ~ Ψ * p * (ia * sin(p * Θ) + ib * sin(p * Θ - 2π / 3) + ic * sin(p * Θ + 2π / 3)),
-
 
         D(ωₘ) ~ (1 / J) * (input1.u - Tₑ - Tf - F * ωₘ),
         D(Θ) ~ ωₘ,
-
 
         pin_a.i ~ -ia,
         pin_b.i ~ -ib,
         pin_c.i ~ -ic,
         pin_a.i + pin_b.i + pin_c.i + pin_n.i ~ 0,
-
 
         output1.u ~ Tₑ,
         output2.u ~ ωₘ,
@@ -81,7 +77,6 @@ function LoadABC(; name, R_L = 10.0)
         pin_b.v - pin_n.v ~ R_L * pin_b.i,
         pin_c.v - pin_n.v ~ R_L * pin_c.i,
 
-
         pin_n.i ~ -(pin_a.i + pin_b.i + pin_c.i),
     ]
 
@@ -104,11 +99,9 @@ function WindResistive(; name, R_L = 10.0, p = 4)
 
         connect(aero.output, gen.input1),
 
-
         connect(gen.pin_a, Aload.pin_a),
         connect(gen.pin_b, Aload.pin_b),
         connect(gen.pin_c, Aload.pin_c),
-
 
         connect(gen.pin_n, gnd.g),
     ]
