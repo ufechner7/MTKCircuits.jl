37140cac0f7a022efc13aed01b6c28d536006bd5
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-05-19 03:51:36 +0200

    Improve menu.jl

--- examples/menu.jl
+++ examples/menu.jl
@@ -2,19 +2,23 @@ using REPL.TerminalMenus
 
 examples_dir = joinpath(@__DIR__)
 example_files = filter(f -> splitext(f)[1] != "menu", sort(readdir(examples_dir)))
-example_names = [splitext(f)[1] for f in example_files]
+example_names = [[splitext(f)[1] for f in example_files]; "quit"]
 
-menu = RadioMenu(example_names, pagesize = 10)
+while true
+    local menu = RadioMenu(example_names, pagesize = 10)
 
-println("Select an example to run:")
-choice = request(menu)
+    println("Select an example to run (or 'quit' to exit):")
+    local choice = request(menu)
 
-if choice == -1
-    println("No selection made. Exiting.")
-else
-    file = joinpath(examples_dir, example_files[choice])
-    rel_path=joinpath("examples", example_files[choice])
-    println("\nRunning: $rel_path")
-    println(repeat("─", length(rel_path)+9))
-    include(file)
+    if choice == -1 || choice == length(example_names)
+        println("Exiting.")
+        break
+    else
+        local file = joinpath(examples_dir, example_files[choice])
+        local rel_path = joinpath("examples", example_files[choice])
+        println("\nRunning: $rel_path")
+        println(repeat("─", length(rel_path) + 9))
+        include(file) |> display
+        println()
+    end
 end
